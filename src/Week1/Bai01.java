69fbe5cd83530d731a41cb75e86731b688fa8fd7
TinyNarwhal <duynguyenkhanh2701@gmail.com>
2026-09-20 11:34:21 +0700

    BTVN Week1

--- src/Week1/Bai01.java
+++ src/Week1/Bai01.java
@@ -5,7 +5,8 @@ public void union(int p, int q) {
         }
     }
 }
-/*cho leader = [0,1,2]
+/*
+cho leader = [0,1,2]
 khi thực hiện union(0,1) thì leader = [1,1,2]
 khi thực hiện union(0,2) thì leader = [2,1,2]
 tuy nhiên find (0) sẽ trả về 2, find(1) sẽ trả về 1, find(2) sẽ trả về 2
--- src/Week1/Bai02.java
+++ src/Week1/Bai02.java
@@ -1,6 +1,6 @@
 /*
 a) co 1 thanh phan lien thong
 b) co n(n-1)/2 lan cap nhat
-c) Θ(n)
-d) Θ(1)
+c) O(n)
+d) O(1)
 */
